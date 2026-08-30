e56a6eab2ea0df5462937e28d89dfd88279c1c57
shriyansh0201-ship-it <shriyansh0102@gmail.com>
2026-08-30 19:14:54 +0530

    Printing multiple values

---
+++ Activity 1/Printing.py
@@ -0,0 +1,8 @@
+#Printing a sentence
+print("Printing is awsome")
+#I am going to print a number
+print(6)
+#Printing multiple values
+print("I", "am", "Shriyansh")
+print("I love sports\n")
+print("I am in Orchids the international school",end="Bolarum branch")
