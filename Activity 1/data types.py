6019f6ea87dee0b900c9cc60976b921a9e298b2b
shriyansh0201-ship-it <shriyansh0102@gmail.com>
2026-09-05 19:38:30 +0530

    anything

---
+++ Activity 1/data types.py
@@ -0,0 +1,7 @@
+name = input("Enter your name")
+gadget = input("Enter your gadget")
+agent_number = 6
+Height = 7.3
+Is_Active = True
+print ("Name",name,"type",type(name))
+Agent = str(agent_number)
